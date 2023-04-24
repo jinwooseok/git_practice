d82938d67aed92ec773581e9a910820a0ecde984
jinwooseok <jinus7949@naver.com>
2023-04-24 21:37:09 +0900

    feat: Print fizz
    Condition: i is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def do_fizzbuzz():
+def do_fizzbuzz(num:int):
     """
     fizzbuzz: print fizz buzz fizzbuzz
     3: fizz
@@ -6,7 +6,14 @@ def do_fizzbuzz():
     15: fizzbuzz
     etc: num
     """
+    for i in range(1,num+1):
+        if i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("buzz")
+        else:
+            print(f'{i}')
     print('hello')
 
 if __name__ == '__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(15)
